765ab9556b4691e6aa8ebc74a40a2c8f57bc3165
Keerthana171003 <115733684+Keerthana171003@users.noreply.github.com>
2023-10-28 21:59:48 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,67 @@
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+let points = 0;
+
+function shuffleCards() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 16);
+        card.style.order = randomPos;
+    });
+}
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        awardPoints(10);
+    } else {
+        unflipCards();
+    }
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    }, 1000);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+function awardPoints(pointsToAdd) {
+    points += pointsToAdd; 
+    document.getElementById('points').textContent = `Points: ${points}`;
+    console.log(`Awarded ${pointsToAdd} points.`);
+}
+
+shuffleCards(); 
+cards.forEach(card => card.addEventListener('click', flipCard));
